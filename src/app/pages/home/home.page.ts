7569fc8832516ee5d7affb95165cf721889632dd
Katie Hull <katie.ann.hull@gmail.com>
2020-07-28 22:39:00 -0700

    removing commented-out code

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { RunProviderService } from 'src/app/model/run-provider.service';
-import { Route } from 'src/app/route';
-import { RouteProgress } from 'src/app/routeProgress';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -10,31 +8,9 @@ import { RouteProgress } from 'src/app/routeProgress';
 })
 export class HomePage {
 
-/*   availableRoutes: Array<Route>;
-  routeProgress: RouteProgress; */
-
   constructor(
-//    private runProviderService: RunProviderService,
+    public alertController: AlertController
   ) { }
 
-  ngOnInit() {
-//    this.getAvailableRoutes();
-  }
-
-  /* getAvailableRoutes() {
-    //calls provider for routes
-    console.log("Getting available routes");
-    this.runProviderService.getAvailableRoutes()
-      .subscribe(routes => this.availableRoutes = routes);
-    console.log(this.availableRoutes);
-    this.availableRoutes.forEach(route => console.log(route.length));
-//    console.log(this.availableRoutes);
-  }
-
-  startRoute(routeId: number) {
-    console.log("starting route");
-    this.runProviderService.startRoute(routeId, 1)
-    .subscribe(routeProgress => this.routeProgress = routeProgress);
-    //wait to redirect until provder has responded?
-  } */
+  ngOnInit() {}
 }
